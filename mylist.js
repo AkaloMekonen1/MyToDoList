c1a17c763b9d3c5002b9736b68a70758459fc014
Akalo Mekonen <mekonen168@gmail.com>
2018-12-31 20:44:10 +0200

    Comparison statement was fixed

--- mylist.js
+++ mylist.js
@@ -2,16 +2,15 @@
 //2. it should have a `name` and `done` properties and a `toggleDone` method which toggles the done property from true to false and vice-versa
 
 //write your code here..
-function ToDoItem(name, done){
-this.name = name;
-this.done = done;  
-} 
-ToDoItem.prototype.toggleDone = function(){
-    if(this.done = 1){
-        this.done = 0;
-    }else if(this.done = 0){
-        this.done = 1; 
+function ToDoItem(name){
+    this.name = name;
+    this.done = true;
 }
+
+ToDoItem.prototype.toggleDone = function(){
+    if (this.done === true){
+        this.done = false;
+    }else this.done = true;
 }
 
 //If you write it correctly the following code should work:
@@ -21,7 +20,7 @@ var toDoItem2 = new ToDoItem('eat breakfast')
 var toDoItem3 = new ToDoItem('go to work')
 
 toDoItem1.toggleDone()
-toDoItem2.toggleDone()
+toDoItem3.toggleDone()
 toDoItem2.toggleDone()
 
 console.log(toDoItem1.name) // should print 'wake up'
