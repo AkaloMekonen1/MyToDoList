2bea575ed7c765da8c60809f90dccb89da872ea7
Akalo Mekonen <mekonen168@gmail.com>
2019-01-13 19:56:09 +0200

    I changed push order from name to item

--- mylist.js
+++ mylist.js
@@ -33,17 +33,17 @@ function List(){
 }
 List.prototype.add = function(name){
     var item = new ToDoItem(name);
-    this.list.push(name)
+    this.list.push(item);
 }
 
 var shoppingList = new List();
 shoppingList.add('milk');
 shoppingList.add('bread');
 shoppingList.add('eggs');
-shoppingList[1].toggleDone()
-console.log(shoppingList[1].done) //should be true
-console.log(shoppingList[1].name) //should be bread
-console.log(shoppingList[2].done) //should be false
-console.log(shoppingList[2].name) //should be eggs
-console.log(shoppingList[0].name) //should be milk
-console.log(shoppingList[0].name) //should be false
+shoppingList[1].toggleDone();
+console.log(shoppingList[1].done); //should be true
+console.log(shoppingList[1].name); //should be bread
+console.log(shoppingList[2].done); //should be false
+console.log(shoppingList[2].name); //should be eggs
+console.log(shoppingList[0].name); //should be milk
+console.log(shoppingList[0].name); //should be false
