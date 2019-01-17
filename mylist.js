1900aafef20f7abf3b2a051650127c5397d5c1d5
Akalo Mekonen <mekonen168@gmail.com>
2019-01-17 20:18:24 +0200

    I created splice to remove this.list array

--- mylist.js
+++ mylist.js
@@ -21,12 +21,12 @@ toDoItem1.toggleDone()
 toDoItem2.toggleDone()
 toDoItem2.toggleDone()
 
-console.log(toDoItem1.name) // should print 'wake up'
-console.log(toDoItem1.done) // should print: true
-console.log(toDoItem2.name) // should print: 'eat breakfast'
-console.log(toDoItem2.done) // should print: false
-console.log(toDoItem3.name) // should print: 'go to work'
-console.log(toDoItem3.done) // should print: false
+// console.log(toDoItem1.name) // should print 'wake up'
+// console.log(toDoItem1.done) // should print: true
+// console.log(toDoItem2.name) // should print: 'eat breakfast'
+// console.log(toDoItem2.done) // should print: false
+// console.log(toDoItem3.name) // should print: 'go to work'
+// console.log(toDoItem3.done) // should print: false
 
 function List(){
     this.list = [];
@@ -37,17 +37,17 @@ List.prototype.add = function(name){
 }
 
 List.prototype.remove = function(item){
-    var removeItem = item.splice();
+    var removeItem = item.splice(this.list, this.list.length);
 }
 
-var shoppingList = new List();
-shoppingList.add('milk');
-shoppingList.add('bread');
-shoppingList.add('eggs');
-shoppingList.list[1].toggleDone();
-console.log(shoppingList.list[1].done); //should be true
-console.log(shoppingList.list[1].name); //should be bread
-console.log(shoppingList.list[2].done); //should be false
-console.log(shoppingList.list[2].name); //should be eggs
-console.log(shoppingList.list[0].name); //should be milk
-console.log(shoppingList.list[0].name); //should be false
+// var shoppingList = new List();
+// shoppingList.add('milk');
+// shoppingList.add('bread');
+// shoppingList.add('eggs');
+// shoppingList.list[1].toggleDone();
+// console.log(shoppingList.list[1].done); //should be true
+// console.log(shoppingList.list[1].name); //should be bread
+// console.log(shoppingList.list[2].done); //should be false
+// console.log(shoppingList.list[2].name); //should be eggs
+// console.log(shoppingList.list[0].name); //should be milk
+// console.log(shoppingList.list[0].name); //should be false
