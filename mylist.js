d08c7634bc0b7d6f48ca088dd0f3b1f69f54cd31
Akalo Mekonen <mekonen168@gmail.com>
2019-03-12 08:36:34 +0200

    add button is created

--- mylist.js
+++ mylist.js
@@ -1,25 +1,11 @@
-//1. create a ToDoItem class
-//2. it should have a `name` and `done` properties and a `toggleDone` method which toggles the done property from true to false and vice-versa
-
-//write your code here..
 function ToDoItem(name){
     this.name = name;
-    this.done = true;
+    this.done = false;
 }
 
 ToDoItem.prototype.toggleDone = function(){
     this.done = !this.done;
 }
-
-//If you write it correctly the following code should work:
-
-// var toDoItem1 = new ToDoItem('wake up')
-// var toDoItem2 = new ToDoItem('eat breakfast')
-// var toDoItem3 = new ToDoItem('go to work')
-
-// toDoItem1.toggleDone()
-// toDoItem2.toggleDone()
-// toDoItem2.toggleDone()
 
 
 
@@ -35,7 +21,7 @@ List.prototype.add = function(name){
 
 List.prototype.remove = function(name){  
     var indexToRemove = this.list.findIndex(function(listItem){
-        // if(this.done === true){ elment = name}
+        
         return listItem.name === name
     });
     this.list.splice(indexToRemove,1);
@@ -46,17 +32,5 @@ List.prototype.toggleDone = function(name){
        return item.name === name
     })
     item.toggleDone()
-    // console.log('item to toggle',item)
 }
 
-var myList = new List();
-myList.add("wake up");
-myList.add("eat");
-myList.add("go to work");
-myList.add("back from work");
-myList.add("go to sleep");
-myList.remove("wake up");
-myList.remove("go to work");
-myList.remove("eat");
-myList.remove("back from work");
-myList.toggleDone('go to sleep');
